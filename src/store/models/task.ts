c033c788462d25863b26c7edd6525977dae42eea
Marvin Zeising <mzeising@bethel.jw.org>
2019-07-10 12:27:53 +0200

    add id to task class

--- src/store/models/task.ts
+++ src/store/models/task.ts
@@ -1,9 +1,13 @@
+import uuidv4 from 'uuid/v4'
+
 export default class Task {
 
+  public id: string
   public name: string
   public command: string
 
   constructor(name: string, command: string) {
+    this.id = uuidv4()
     this.name = name
     this.command = command
   }
