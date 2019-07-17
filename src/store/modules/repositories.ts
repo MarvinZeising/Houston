5d9ec08e4383298fdcce8dc1931ad8e4cc1ba029
Marvin Zeising <mzeising@bethel.jw.org>
2019-07-17 15:30:41 +0200

    add a whole bunch of other tasks

--- src/store/modules/repositories.ts
+++ src/store/modules/repositories.ts
@@ -15,14 +15,40 @@ export default class RepositoryModule extends VuexModule {
   @MutationAction({ mutate: ['repositories'] })
   public async initRepositoryModule() {
     const hub = new Repository('HuB', 'C:\\src\\HuB\\Admin', [
-      new Task('Run Dev Server', TaskType.DefiniteWithNotifications, '.\\ci.cmd rundevserver'),
-      new Task('Run Agent 3', TaskType.DefiniteWithNotifications, '.\\ci.cmd servertests 3'),
+      new Task('Show changes', TaskType.Definite, 'gst; git diff'),
+      new Task('Start Dev Server', TaskType.DefiniteWithNotifications, '.\\ci.cmd rundevserver'),
+      new Task('Start Dev Client', TaskType.DefiniteWithNotifications, '.\\ci.cmd rundevclient'),
+      new Task('Test Agent 3', TaskType.DefiniteWithNotifications, '.\\ci.cmd servertests 3'),
+      new Task('Test Unittests', TaskType.DefiniteWithNotifications, '.\\ci.cmd server'),
+      new Task('Gitgreen', TaskType.Definite, 'gitgreen'),
     ])
-    const personMesh = new Repository('Person Mesh', 'C:\\src\\Mesh\\Person', [
+    const personMesh = new Repository('Person Mesh', 'C:\\src\\cdh\\mesh\\person', [
+      new Task('Show changes', TaskType.Definite, 'gst; git diff'),
+      new Task('Show branches', TaskType.Definite, 'git br'),
       new Task('Run Service', TaskType.Continuous, 'cd .\\src\\JW.Mesh.Person.Service; dotnet watch run'),
       new Task('Run Subscriber', TaskType.Continuous, 'cd .\\src\\JW.Mesh.Person.Subscriber; dotnet watch run'),
+      new Task('Setup', TaskType.Definite, 'sca'),
+      new Task('Pull', TaskType.Definite, 'git br -u master; gf'),
     ])
-    return { repositories: [ hub, personMesh ] }
+    const serviceBus = new Repository('Service Bus', 'C:\\src\\cdh\\service-bus', [
+      new Task('Show changes', TaskType.Definite, 'gst; git diff'),
+      new Task('Show branches', TaskType.Definite, 'git br'),
+      new Task('Pull', TaskType.Definite, 'git br -u master; gf'),
+      new Task(
+        'Make new branch',
+        TaskType.Definite,
+        '',
+        (name: string) => `git co master; git br -u origin/master; git pull; git co -b ${name}`),
+    ])
+    const db = new Repository('Database', 'C:\\src\\HuB\\Database', [
+      new Task('Show changes', TaskType.Definite, 'gst; git diff'),
+      new Task('Pull', TaskType.Definite, 'git br -u master; gf'),
+    ])
+    const hubWiki = new Repository('HuB Wiki', 'C:\\src\\HuB\\Admin.wiki', [
+      new Task('Open Code', TaskType.Definite, 'code .'),
+      new Task('Pull', TaskType.Definite, 'gf'),
+    ])
+    return { repositories: [ hub, personMesh, serviceBus, db, hubWiki ] }
   }
 
   @Action({ commit: 'setRepositories' })
